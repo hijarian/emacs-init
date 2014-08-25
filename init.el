1fd136f42b6123b20629bb6a0dad4723d8622d13
Mark Safronov <hijarian@gmail.com>
2014-08-26 02:16:03 +0400

    Removed unused block of code

--- init.el
+++ init.el
@@ -166,40 +166,6 @@
 ;; least of text you've pasted into it in the first place.
 (setq yank-pop-change-selection t)  ; makes rotating the kill ring change the X11 clipboard.
 
-;-----------------------------------------------------------------------
-; 5. ANSI TERM KEYBINDING
-
-; Enabling creating shell buffers by F2. If it exists, just switch to it.
-(require 'term)
-(defun visit-ansi-term () 
-  "If the current buffer is:
-   1) a running ansi-term named `*ansi-term*', rename it.
-   2) a stopped ansi-term, kill it and create a new one.
-   3) a non ansi-term, go to an already running ansi-term
-      or start a new one while killing a defunt one"
-  (interactive)
-  (let ((is-term (string= "term-mode" major-mode))
-        (is-running (term-check-proc (buffer-name)))
-        (term-cmd "/bin/bash")
-        (anon-term (get-buffer "*ansi-term*")))
-    (if is-term
-        (if is-running
-            (if (string= "*ansi-term*" (buffer-name))
-                (call-interactively 'rename-buffer)
-              (if anon-term
-                  (switch-to-buffer "*ansi-term*")
-                (ansi-term term-cmd)))
-          (kill-buffer (buffer-name))
-          (ansi-term term-cmd))
-      (if anon-term
-          (if (term-check-proc "*ansi-term*")
-              (switch-to-buffer "*ansi-term*")
-            (kill-buffer "*ansi-term*")
-            (ansi-term term-cmd))
-        (ansi-term term-cmd)))))
-(global-set-key (kbd "<f2>") 'visit-ansi-term)
-(put 'downcase-region 'disabled nil)
-
 ;---------------------------------------------------------------------------------------------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
