6cbbde644e8b289b6f22cc277498a4aff04f52bb
Mark Safronov <hijarian@gmail.com>
2015-04-22 21:12:23 +0300

    Enabled overwriting the saved desktop settings ALWAYS.

--- custom.el
+++ custom.el
@@ -5,7 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(column-number-mode t)
- '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(custom-safe-themes
+   (quote
+	("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(desktop-load-locked-desktop t)
+ '(desktop-save t)
+ '(desktop-save-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
